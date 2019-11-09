a1205cbc02a66024e2c48cd9a07d959fcea60153
Carter Rennick <carter.rennick@gmail.com>
2019-11-09 04:07:51 -0500

    * Fixed newline behaviour to be consistent with SRC's current formatting

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ function run(fileName){
 	var converter = new showdown.Converter();
 	
 	jQuery.get('https://raw.githubusercontent.com/ArkhamSpeedrunningWiki/ArkhamSpeedrunningWiki.github.io/master/' + fileName, function(data){
+		data = data.replace(/(?:\r\n|\r|\n)/g, '<br />');
+		console.log(data);
 		target.innerHTML = converter.makeHtml(data);
 	});
 }
