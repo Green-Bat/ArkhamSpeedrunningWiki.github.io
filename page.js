6200dbb152fd2838a18f15cde966343753432d12
Carter Rennick <carter.rennick@gmail.com>
2019-11-10 18:53:34 -0500

    Major formatting changes

--- page.js
+++ page.js
@@ -27,14 +27,10 @@ function ProcessLinks(text){
 				newUrl = newUrl.replace('youtu.be/', 'youtube.com/embed/');
 			}
 			
-			newText = newText.replace(url, '<iframe width="720" height="400" src=' + newUrl + '></iframe>');
+			newText = newText.replace(url, '<iframe width="720" height="480" src=' + newUrl + '></iframe>');
 		}else if(IsTwitchLink(url) && !url.includes('/p/')){
-			console.log(url);
-			
 			var newURL = url.substring(url.lastIndexOf('/') + 1);
-			console.log(newURL);
-
-			newText = newText.replace(url, '<iframe width="720" height="400" src=https://player.twitch.tv/?video=' + newURL + '&autoplay=false></iframe>');
+			newText = newText.replace(url, '<iframe width="720" height="480" src=https://player.twitch.tv/?video=' + newURL + '&autoplay=false></iframe>');
 		}
 	});
 
@@ -55,6 +51,6 @@ function LoadMarkdown(){
 	jQuery.get('https://raw.githubusercontent.com/ArkhamSpeedrunningWiki/ArkhamSpeedrunningWiki.github.io/master/' + fileName + '.md', function(data){
 		data = ProcessLinks(data);
 		data = ProcessNewLines(data);
-		target.innerHTML = converter.makeHtml(data);
+		target.innerHTML = "<div class=main-panel>" + converter.makeHtml(data) + "</div>";
 	});
 }
