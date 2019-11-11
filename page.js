305304f4806b996ade9c2ce04e29704cf9343067
Carter Rennick <carter.rennick@gmail.com>
2019-11-10 20:09:38 -0500

    Minor code improvements

--- page.js
+++ page.js
@@ -1,36 +1,36 @@
 function IsYouTubeLink(text){
-	return text.includes('youtube') || text.includes('youtu.be');
+	return text.includes("youtube") || text.includes("youtu.be");
 }
 
 function IsTwitchLink(text){
-	return text.includes('twitch.tv');
+	return text.includes("twitch.tv");
 }
 
 function ProcessLinks(text){
 	var newText = text;
 
 	var result = URI.withinString(text, function(url){
-		if(url.includes('http') && !url.includes('https')){
-			url.replace('http', 'https');
+		if(url.includes("http") && !url.includes("https")){
+			url.replace("http", "https");
 		}
-		
+
 		if(IsYouTubeLink(url)){
 			var newUrl = url;
 			
-			if(url.includes('&')){
-				newUrl = url.substr(0, url.lastIndexOf('&'));
+			if(url.includes("&")){
+				newUrl = url.substr(0, url.lastIndexOf("&"));
 			}
 			
-			if(newUrl.includes('watch?v=')){
-				newUrl = newUrl.replace('watch?v=', 'embed/');
-			}else if(url.includes('youtu.be')){
-				newUrl = newUrl.replace('youtu.be/', 'youtube.com/embed/');
+			if(newUrl.includes("watch?v=")){
+				newUrl = newUrl.replace("watch?v=", "embed/");
+			}else if(url.includes("youtu.be")){
+				newUrl = newUrl.replace("youtu.be/", "youtube.com/embed/");
 			}
 			
-			newText = newText.replace(url, '<iframe width="720" height="480" src=' + newUrl + '></iframe>');
-		}else if(IsTwitchLink(url) && !url.includes('/p/')){
-			var newURL = url.substring(url.lastIndexOf('/') + 1);
-			newText = newText.replace(url, '<iframe width="720" height="480" src=https://player.twitch.tv/?video=' + newURL + '&autoplay=false></iframe>');
+			newText = newText.replace(url, "<iframe width='720' height='480' src=" + newUrl + "></iframe>");
+		}else if(IsTwitchLink(url) && !url.includes("/p/")){
+			var newURL = url.substring(url.lastIndexOf("/") + 1);
+			newText = newText.replace(url, "<iframe width='720' height='480' src=https://player.twitch.tv/?video=" + newURL + "&autoplay=false></iframe>");
 		}
 	});
 
@@ -41,15 +41,15 @@ function LoadMarkdown(){
 	var url = new URL(window.location.href);
 	var fileName = url.searchParams.get("page");
 	
-	if(fileName == null){
+	if(fileName === null){
 		return;
 	}
 	
-	var target = document.getElementById('targetDiv');
+	var target = document.getElementById("targetDiv");
 	var converter = new showdown.Converter();
-	converter.setFlavor('github');
+	converter.setFlavor("github");
 	
-	jQuery.get('https://raw.githubusercontent.com/ArkhamSpeedrunningWiki/ArkhamSpeedrunningWiki.github.io/master/' + fileName + '.md', function(data){
+	jQuery.get("https://raw.githubusercontent.com/ArkhamSpeedrunningWiki/ArkhamSpeedrunningWiki.github.io/master/" + fileName + ".md", function(data){
 		data = ProcessLinks(data);
 		data = converter.makeHtml(data);
 		target.innerHTML = "<div class=main-panel>" + data + "</div>";
